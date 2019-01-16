e6a3c49dbb9d9467d6fbbe396967f49553c45e08
书语湘镡 <13686821736@163.com>
2019-01-16 14:06:20 +0800

    新增提交257

--- test/element_accept.py
+++ test/element_accept.py
@@ -12,9 +12,9 @@ size = driver.find_element_by_id("kw").size
 #定位到输入框按钮并且获取按钮上的文本
 text = driver.find_element_by_css_selector("input[type='submit']").text
 #返回元素的属性值，可以是id、name、type 或元素拥有的其它任意属性
-attribute = driver.find_element_by_css_selector("input[type='submit']").get_attribute('type')
+attribute = driver.find_element_by_id("kw").get_attribute('type')
 #返回元素的css属性值
-css_attribute = driver.find_element_by_link_text("新闻").value_of_css_property("font-size")
+css_attribute = driver.find_element_by_id("kw").value_of_css_property("font-size")
 #返回元素的结果是否可见，返回结果为True 或False
 result = driver.find_element_by_css_selector("input[type='submit']").is_displayed()
 time.sleep(2)
